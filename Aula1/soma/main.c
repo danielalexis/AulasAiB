f2aa5fd4b981f123378de18b6c5dea692bc59143
Daniel Aléxis <danielalexis.o.pereira@gmail.com>
2021-09-27 13:38:30 +0200

    Exercicio 1

---
+++ Aula1/soma/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int num1, num2, soma;
+
+int main()
+{
+    printf("Numero 1: ");
+    scanf("%i", &num1);
+    printf("Numero 2: ");
+    scanf("%i", &num2);
+    soma = num1 + num2;
+    printf("A soma e: %i", soma);
+    return 0;
+}
