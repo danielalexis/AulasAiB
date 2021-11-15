d2c8f63dd5183085156247cf487056e392f66f7f
danielalexis <danielalexis.o.pereira@gmail.com>
2021-11-15 17:11:54 +0000

    Novo Codigo

--- Trabalho1/JogoDaAdivinha/main.c
+++ Trabalho1/JogoDaAdivinha/main.c
@@ -3,82 +3,95 @@
 #include <time.h>
 #include <stdbool.h>
 
-int num_aleatorio, dificuldade, num, tentativas;
-bool acertou = false;
-
+int dificuldade, num, tentativas;
+bool repetir = true, acertou = false;
+char jogar;
 int main()
 {
-    printf("\n    |-----------------------|\n");
-    printf("    |    Jogo da Adivinha   |\n");
-    printf("    |      Dificuldade:     |\n");
-    printf("    |      1 - Facil        |\n");
-    printf("    |      2 - Medio        |\n");
-    printf("    |      3 - Dificil      |\n");
-    printf("    |      4 - Sair         |\n");
-    printf("    |-----------------------|\n");
-    while (!num_aleatorio) {
-        // O jogador escolhe a dificuldade
-        printf("Dificuldade: ");
-        scanf("%i", &dificuldade);
-        //inciar o Random Number Generator
-        srand(time(NULL));
+    while (repetir) {
+        int num_aleatorio;
 
-        // Escolher o numero aleatorio
+        //system("cls"); //limpa o ecrï¿½
+        printf("\n    |-----------------------|\n");
+        printf("    |    Jogo da Adivinha   |\n");
+        printf("    |      Dificuldade:     |\n");
+        printf("    |      1 - Facil        |\n");
+        printf("    |      2 - Medio        |\n");
+        printf("    |      3 - Dificil      |\n");
+        printf("    |      4 - Sair         |\n");
+        printf("    |-----------------------|\n");
+        while (!tentativas) {
+            // O jogador escolhe a dificuldade
+            printf("Dificuldade: ");
+            scanf("%i", &dificuldade);
+            //inciar o Random Number Generator
+            srand(time(NULL));
 
-        // Tabela de dificuldade:
-        // Facil - Numero entre 1 a 15
-        // Medio - Numero entre 1 a 30
-        // Dificl - Numero entre 1 a 60
-        switch (dificuldade){
-            case 1:
-                num_aleatorio = rand() % 15 + 1;
-                tentativas = 3;
-                break;
-            case 2:
-                num_aleatorio = rand() % 30 + 1;
-                tentativas = 6;
-                break;
-            case 3:
-                num_aleatorio = rand() % 60 + 1;
-                tentativas = 9;
-                break;
-            case 4:
-                printf("Adeus");
-                exit(1);
-                break;
-            default:
-                printf("Opcao invalida\n"); //Se a opção for invalida repete até ao jogador escolher uma opção valida
+            // Escolher o numero aleatorio
+
+            // Tabela de dificuldade:
+            // Facil - Numero entre 1 a 15
+            // Medio - Numero entre 1 a 30
+            // Dificl - Numero entre 1 a 60
+            switch (dificuldade){
+                case 1:
+                    num_aleatorio = rand() % 15 + 1;
+                    tentativas = 4;
+                    break;
+                case 2:
+                    num_aleatorio = rand() % 30 + 1;
+                    tentativas = 8;
+                    break;
+                case 3:
+                    num_aleatorio = rand() % 60 + 1;
+                    tentativas = 12;
+                    break;
+                case 4:
+                    printf("Adeus");
+                    exit(1);
+                    break;
+                default:
+                    printf("Opcao invalida\n"); //Se a opcÃ£o for invalida repete atÃ© ao jogador escolher uma opcÃ£o valida
+            }
         }
+        printf("\nNota: escreva '0' para sair do programa\n");
+        printf("Tens %i tentativas\n", tentativas);
+        while (!acertou) {
+            if (tentativas == 0) {
+                break; //sai do loop se o jogador ficou sme tentativas
+            }
+            printf("Adivinha: ");
+            scanf("%i", &num);
+
+            if (num == 0) {
+                break;
+            }
+
+            if (num == num_aleatorio) {
+                acertou = true; //o jogador ganhou
+            } else if (num > num_aleatorio && tentativas != 1) { // Se sï¿½ tiver 1 tentativa nï¿½o mostrar a dica
+                printf("Dica o numero e menor do que %i\n", num);
+            } else if (num < num_aleatorio && tentativas != 1) {
+                printf("Dica o numero e maior do que %i\n", num); // Se sï¿½ tiver 1 tentativa nï¿½o mostrar a dica
+            }
+            tentativas--; // Remover 1 tentativa
+        }
+        if (acertou) {
+            printf("Ganhou!!!\n");
+        } else if (num == 0) {
+            printf("O numero aleatorio era %i\n", num_aleatorio);
+            printf("Adeus\n");
+        } else {
+            printf("Ficou sem tentativas! Boa sorte da proxima!\n");
+
+        }
+            printf("Quer jogar novamente (s/n)? ");
+            scanf(" %c", &jogar);
+            if (jogar == 'n') {
+                repetir = false;
+                break;
+            }  
     }
-    printf("\nNota: escreva '0' para sair do programa\n");
-    printf("Tens %i tentativas\n", tentativas);
-    while (!acertou) {
-        if (tentativas == 0) {
-            break; //sai do loop se o jogador ficou sme tentativas
-        }
-        printf("Adivinha: ");
-        scanf("%i", &num);
 
-        if (num == 0) {
-            break;
-        }
-
-        if (num == num_aleatorio) {
-            acertou = true; //o jogador ganhou
-        } else if (num > num_aleatorio && tentativas != 1) { // Se só tiver 1 tentativa não mostrar a dica
-            printf("Dica o numero e menor do que %i\n", num);
-        } else if (num < num_aleatorio && tentativas != 1) {
-            printf("Dica o numero e maior do que %i\n", num); // Se só tiver 1 tentativa não mostrar a dica
-        }
-        tentativas--; // Remover 1 tentativa
-    }
-    if (acertou) {
-        printf("Ganhou!!!");
-    } else if (num == 0) {
-        printf("O numero aleatorio era %i", num_aleatorio);
-        printf("\nAdeus");
-    } else {
-        printf("Ficou sem tentativas! Boa sorte da proxima!");
-    }
     return 0;
 }
