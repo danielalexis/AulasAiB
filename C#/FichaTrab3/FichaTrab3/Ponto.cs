10506782af942951dc8982aefe89600f7e2c3b87
danielalexis <danielalexis.o.pereira@gmail.com>
2022-02-21 12:12:17 +0000

    codigo

--- C#/FichaTrab3/FichaTrab3/Ponto.cs
+++ C#/FichaTrab3/FichaTrab3/Ponto.cs
@@ -19,5 +19,40 @@ namespace FichaTrab3 {
             //d = sqrt((x2 - x1)^2 + (y2 - y1)^2)
             return Math.Sqrt(Math.Pow(x - p.x, 2) + Math.Pow(y - p.y, 2));
         }
+    
     }
+
+    class Poligono {
+        public Ponto[] pontos;
+        public Poligono(Ponto[] pontos)
+        {
+            this.pontos = pontos;
+        }
+        public void Imprimir()
+        {
+            for (int i = 0; i < pontos.Length; i++)
+            {
+                pontos[i].Imprimir();
+            }
+        }
+        public double Perimetro()
+        {
+            double perimetro = 0;
+            for (int i = 0; i < pontos.Length; i++)
+            {
+                perimetro += pontos[i].Distancia(pontos[(i + 1) % pontos.Length]);
+            }
+            return perimetro;
+        }
+        public double Area()
+        {
+            double area = 0;
+            for (int i = 0; i < pontos.Length; i++)
+            {
+                area += (pontos[i].x * pontos[(i + 1) % pontos.Length].y) - (pontos[i].y * pontos[(i + 1) % pontos.Length].x);
+            }
+            return area / 2;
+        }
+    }
+
 }
